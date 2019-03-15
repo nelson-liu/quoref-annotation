2e5ee09bc140daf6e973417a515d62319e0b149a
Nelson Liu <nfliu@cs.washington.edu>
2019-03-14 20:47:27 -0700

    Fix highlighting

--- sample-span-highlight.js
+++ sample-span-highlight.js
@@ -7,46 +7,45 @@ function reset_higlight() {
 }
 
 function highlight_q1() {
-    document.getElementById("span1a").classList.add("highlighted");
-    document.getElementById("span1b").classList.add("highlighted");
+    $(".span1a").addClass("highlighted");
+    $(".span1b").addClass("highlighted");
 }
 
 function highlight_q2() {
-    document.getElementById("span2a").classList.add("highlighted");
-    document.getElementById("span2b").classList.add("highlighted");
+    $(".span2a").addClass("highlighted");
+    $(".span2b").addClass("highlighted");
 }
 
 function highlight_q3() {
-    document.getElementById("span3a").classList.add("highlighted");
-    document.getElementById("span34b").classList.add("highlighted");
-    document.getElementById("span3c").classList.add("highlighted");
+    $(".span3a").addClass("highlighted");
+    $(".span34b").addClass("highlighted");
+    $(".span3c").addClass("highlighted");
 }
 
 function highlight_q4() {
-    document.getElementById("span4a").classList.add("highlighted");
-    document.getElementById("span34b").classList.add("highlighted");
+    $(".span4a").addClass("highlighted");
+    $(".span34b").addClass("highlighted");
 }
 
 function highlight_q5() {
     // spans being highlighted are the same as those for Q4
-    document.getElementById("span4a").classList.add("highlighted");
-    document.getElementById("span34b").classList.add("highlighted");
+    $(".span4a").addClass("highlighted");
+    $(".span34b").addClass("highlighted");
 }
 
 function highlight_q6() {
-    document.getElementById("span6a").classList.add("highlighted");
-    document.getElementById("span6b").classList.add("highlighted");
+    $(".span6a").addClass("highlighted");
+    $(".span6b").addClass("highlighted");
 }
 
 function highlight_q7() {
-    document.getElementById("span7a").classList.add("highlighted");
-    document.getElementById("span7b").classList.add("highlighted");
+    $(".span7a").addClass("highlighted");
+    $(".span7b").addClass("highlighted");
 }
 
 function highlight_q9() {
-    document.getElementById("span9a").classList.add("highlighted");
-    document.getElementById("span3c").classList.add("highlighted");
-    document.getElementById("span9b").classList.add("highlighted");
-    document.getElementById("span9c").classList.add("highlighted");
-    document.getElementById("span9d").classList.add("highlighted");
+    $(".span9a").addClass("highlighted");
+    $(".span9b").addClass("highlighted");
+    $(".span9c").addClass("highlighted");
+    $(".span9d").addClass("highlighted");
 }
